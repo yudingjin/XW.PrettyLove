89d2518c92f99b8bcc6167530e56ad679ebe3d6a
yudingjin <yudingjin@163.com>
2025-11-16 14:52:01 +0800

    1

---
+++ src/Web/XW.PrettyLove.Application.Contract/Services/ISmsAppService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XW.PrettyLove.Application
+{
+    public interface ISmsAppService
+    {
+        Task<bool> SendSmsAsync(string phone, string content);
+    }
+}
