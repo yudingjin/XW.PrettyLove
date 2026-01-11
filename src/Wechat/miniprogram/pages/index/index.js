c00138da4f162d4facbefe1af1b00720bc3eb74b
yushuang <yudingjin@163.com>
2026-01-11 11:17:37 +0800

    1

--- src/Wechat/miniprogram/pages/index/index.js
+++ src/Wechat/miniprogram/pages/index/index.js
@@ -1,2 +1,24 @@
 // index.js
-Page({})
+Page({
+  data: {
+    avatarUrl: '../../assets/left.jpg',
+    nickname: ''
+  },
+  //获取微信头像
+  chooseavator(event) {
+    //目前获取的微信头像是临时路径
+    //临时路径是有失效时间的，在实际开发中，需要将临时路径上传到公司服务器
+    const { avatarUrl } = event.detail
+    console.log(event)
+    this.setData({
+      avatarUrl: avatarUrl
+    })
+  },
+  //获取微信昵称
+  onSubmit(event) {
+    const { nickname } = event.detail.value
+    this.setData({
+      nickname: nickname
+    })
+  }
+})
