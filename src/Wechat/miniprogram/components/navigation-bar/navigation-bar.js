0af513e0748bdad9da317d863dd3afbbe926bf36
yushuang <yudingjin@163.com>
2026-01-11 09:40:43 +0800

    1

--- src/Wechat/miniprogram/components/navigation-bar/navigation-bar.js
+++ src/Wechat/miniprogram/components/navigation-bar/navigation-bar.js
@@ -60,17 +60,15 @@ Component({
   lifetimes: {
     attached() {
       const rect = wx.getMenuButtonBoundingClientRect()
-      wx.getSystemInfo({
-        success: (res) => {
-          const isAndroid = res.platform === 'android'
-          const isDevtools = res.platform === 'devtools'
-          this.setData({
-            ios: !isAndroid,
-            innerPaddingRight: `padding-right: ${res.windowWidth - rect.left}px`,
-            leftWidth: `width: ${res.windowWidth - rect.left }px`,
-            safeAreaTop: isDevtools || isAndroid ? `height: calc(var(--height) + ${res.safeArea.top}px); padding-top: ${res.safeArea.top}px` : ``
-          })
-        }
+      const platform = (wx.getDeviceInfo() || wx.getSystemInfoSync()).platform
+      const isAndroid = platform === 'android'
+      const isDevtools = platform === 'devtools'
+      const { windowWidth, safeArea: { top = 0, bottom = 0 } = {} } = wx.getWindowInfo() || wx.getSystemInfoSync()
+      this.setData({
+        ios: !isAndroid,
+        innerPaddingRight: `padding-right: ${windowWidth - rect.left}px`,
+        leftWidth: `width: ${windowWidth - rect.left}px`,
+        safeAreaTop: isDevtools || isAndroid ? `height: calc(var(--height) + ${top}px); padding-top: ${top}px` : ``
       })
     },
   },
@@ -78,13 +76,12 @@ Component({
    * 组件的方法列表
    */
   methods: {
-    _showChange(show: boolean) {
+    _showChange(show) {
       const animated = this.data.animated
       let displayStyle = ''
       if (animated) {
-        displayStyle = `opacity: ${
-          show ? '1' : '0'
-        };transition:opacity 0.5s;`
+        displayStyle = `opacity: ${show ? '1' : '0'
+          };transition:opacity 0.5s;`
       } else {
         displayStyle = `display: ${show ? '' : 'none'}`
       }
